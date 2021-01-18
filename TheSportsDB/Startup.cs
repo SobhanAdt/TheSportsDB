e93712b495e0a27e5e0a5f807502bf130ae847e4
Ali Mhmdi <karanous@github.com>
2021-01-18 23:36:52 +0330

    ali commit 2

--- TheSportsDB/Startup.cs
+++ TheSportsDB/Startup.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TheSportsDB.HttpClinet;
+using TheSportsDB.HttpClinet.Ali;
 using TheSportsDB.HttpClinet.Rahbar;
 using TheSportsDB.Repositores;
 using TheSportsDB.Services;
