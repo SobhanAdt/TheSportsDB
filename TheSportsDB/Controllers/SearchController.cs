f90f90ddc543b0e0669a9a011de7e2aed56e90a4
Your Name <you@example.com>
2021-01-18 14:58:24 +0330

    2

---
+++ TheSportsDB/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheSportsDB.HttpClinet;
+using TheSportsDB.Models;
+
+namespace TheSportsDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly HttpClinetSport clinet;
+
+        public SearchController(HttpClinetSport clinet)
+        {
+            this.clinet = clinet;
+        }
+
+        [HttpGet]
+        public List<TeamName> GetByName([FromQuery]string teamName)
+        {
+            return clinet.GetTeamByName(teamName);
+        }
+    }
+}
--- TheSportsDB/HttpClinet/HttpClinetSport.cs
+++ TheSportsDB/HttpClinet/HttpClinetSport.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Threading.Tasks;
 using TheSportsDB.Models;
 using TheSportsDB.Services;
+using static TheSportsDB.Models.TeamName;
 
 namespace TheSportsDB.HttpClinet
 {
@@ -40,6 +42,37 @@ namespace TheSportsDB.HttpClinet
 
                 var resultService = JsonSerializer.Deserialize<SportNameLst>(stringContent);
                 result = resultService.sports.Select(x => new SportName { strSport = x.strSport }).ToList();
+            }
+            return result;
+        }
+
+        public List<TeamName> GetTeamByName(string teamName)
+        {
+            var httpResponse = client.GetAsync($"api/v1/json/1/searchteams.php?t={teamName}").Result;
+            httpResponse.EnsureSuccessStatusCode();
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            List<TeamName> result;
+            using (HttpContent content = httpResponse.Content)
+            {
+
+                string stringContent = content.ReadAsStringAsync()
+                    .Result;
+
+                var resultService = JsonSerializer.Deserialize<TeamList>(stringContent);
+                var resulTeamNames = resultService.teams.Select(x => new TeamName()
+                {
+                    Team = x.strTeam,
+                    Alternate = x.strAlternate,
+                    DescriptionEN = x.strDescriptionEN,
+                    FormedYear = x.intFormedYear,
+                    League = x.strLeague,
+                    Stadium = x.strStadium
+                }).ToList();
+                result = resulTeamNames.Take(2).ToList();
             }
             return result;
         }
